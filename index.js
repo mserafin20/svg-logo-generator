3a6284c18349f03bf33145c49c3f6a2dcffe12d1
mserafin20 <mmserafin20@gmail.com>
2023-05-25 22:27:14 -0400

    set up inquirer prompt questions and made sure they worked within the terminal

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+function init() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "text",
+            message: "Enter up to three characters of your choosing: "
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "Enter a keyword for a color or a hexidecimal number to determine the color of your text: "
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "Please choose a shape from the list: ",
+            choices: ['Square', 'Triangle', 'Circle'],
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "Enter a keyword or hexidecimal number to determine the color of your shape: "
+        },
+    ])
+}
+
+init();
